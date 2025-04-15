46a3b262744889d346052f604f0e260d52493e67
Chinnapa Raju <rajuajuraj640@gmail.com>
2025-04-15 14:59:28 +0530

    Added new sql file

--- src/main/java/com/net/services/CustomerLoginService.java
+++
@@ -1,8 +0,0 @@
-package com.net.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CustomerLoginService {
-    boolean CustomerLogin(Integer customerid, String customerpassword);
-}
